4df4680815f59e77ee7382e40e54810b5bac98a3
saqibahmed13 <saqiba0484@gmail.com>
2024-02-16 19:25:17 +0530

    added

--- react-basic/src/Product.jsx
+++ react-basic/src/Product.jsx
@@ -1,9 +1,10 @@
 
 
 function Product({title,price,features}){
+    let color = {backgroundColor: price>30000 ? "yellow" : ""}
     const list = features.map((singleValue)=> <li>{singleValue}</li>)
     return (
-        <div>
+        <div style={color}>
             <h1>{title}</h1>
             <h5>Price:{price}</h5>
             <p> {list} </p>
--- react-basic/src/ProductList.jsx
+++ react-basic/src/ProductList.jsx
@@ -1,14 +1,15 @@
 import Product from "./Product.jsx";
 
 function ProductList(){
+    
     let options = ["hiTech","cheap", "durable"];
     
     return (
         <div>
-            <Product title = "Laptop" price="30000" features = {options}/>
+            <Product title = "Laptop" price="40000" features = {options}/>
             {/* <Product title="phone" price="20000"/>
             <Product title="accessories" price="10000"/> */}
-            
+
         </div>
     );
 }
