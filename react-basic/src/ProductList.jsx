7014b50d5cdc43dbd790b518814f5dcca81f74d1
saqibahmed13 <saqiba0484@gmail.com>
2024-02-16 19:10:52 +0530

    added

--- react-basic/src/ProductList.jsx
+++ react-basic/src/ProductList.jsx
@@ -8,6 +8,7 @@ function ProductList(){
             <Product title = "Laptop" price="30000" features = {options}/>
             {/* <Product title="phone" price="20000"/>
             <Product title="accessories" price="10000"/> */}
+            
         </div>
     );
 }
