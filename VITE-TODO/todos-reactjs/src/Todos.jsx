5b2bc87961e8cd175005124da4abb6e63e142228
saqibahmed13 <saqiba0484@gmail.com>
2024-02-23 14:38:47 +0530

    added

---
+++ VITE-TODO/todos-reactjs/src/Todos.jsx
@@ -0,0 +1,12 @@
+export default function Todos(){
+
+
+    return (
+    <div>
+<input type="text" placeholder="Add Task" />
+<button>Add Task</button>
+
+<button>Added Tasks</button>
+    </div>
+    )
+}
--- react-todo-basic/src/TodoList.jsx
+++ react-todo-basic/src/TodoList.jsx
@@ -1,21 +1,55 @@
 import { useState } from "react"
-import {v4 as uuidv4} from 'uuid';
+import {v4 as uuidv4} from 'uuid'; // this is for keys
 
 export default function TodoList(){
     // const [todos, setTodos] = useState(["newSample"]);
-    const [todos, setTodos] = useState([{task:"newsample", id:uuidv4()}]);
-    const [newTodoTask, setNewTodoTask] = useState("");
+    const [todos, setTodos] = useState([{task:"newsample", id:uuidv4()}]); // yeh added wala task ka 
+    const [newTodoTask, setNewTodoTask] = useState("");   // yeh search box ka 
 
-let newTask = ()=>{
-setTodos([...todos, { task:newTodoTask, id:uuidv4() }]);
-setNewTodoTask("");    // search should be empty after adding the task 
+let newTask = ()=> {
+setTodos((prevTodos)=> {
+    return[...prevTodos, { task: newTodoTask, id: uuidv4() } ];
+});
+    setNewTodoTask(" "); 
 
-}
+     // search should be empty after adding the task 
+};
 
 let updateTodo = (event)=>{
 setNewTodoTask(event.target.value);    // target is input uski value
 
 }
+
+let deleteTodo = (id) => {
+    setTodos((prevTodos)=>todos.filter((prevTodos)=> prevTodos.id!=id));
+};
+
+
+let upperCase = () => {
+        setTodos((todos)=>{
+            return todos.map((todo)=>{
+                return {
+                    ...todo, task:todo.task.toUpperCase()
+                }
+            })
+})
+}
+
+
+let deleteTodoOne = (id) => {
+    setTodos((todos) =>
+        todos.map((todo) => {
+            if (todo.id === id) {
+                return {
+                    ...todo,
+                    task: todo.task.toUpperCase()
+                };
+            } else {
+                return todo;   // kisa tha wisach return honde 
+            }
+        })
+    );
+};
 
     return(
         <div>
@@ -24,15 +58,20 @@ setNewTodoTask(event.target.value);    // target is input uski value
             <br />
             <br />
             <br />
-
             <p>Task to do</p>
             <ul>
                 {
                     todos.map((todo)=>{
-                    return <li key={todo.id}>{todo.task}</li>
+                    return <li key={todo.id}>
+                        {todo.task}
+                        <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
+                        <button onClick={()=>deleteTodoOne(todo.id)}>upperCase One</button></li>
+
                     })
                 }
             </ul>
+            <br />
+            <button onClick = {upperCase}> Upper Case </button>
         </div>
     );
 }
